773ba5bb3d14ea65641bec988685fd567b61e8a1
udodinho <udodinho200@gmail.com>
2024-09-30 15:24:50 +0100

    fix: wrap app to context provided

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
+import { WalletConnectProvider } from "./context/walletContext.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <WalletConnectProvider>
     <App />
+    </WalletConnectProvider>
   </StrictMode>,
 )
